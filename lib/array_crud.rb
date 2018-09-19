1f028c03d27a879b604a3aff486883ed0dcf54ec
Veniel Dorestave <veniel@noukod.com>
2018-09-19 15:09:11 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,36 +1,36 @@
 def create_an_empty_array
-  my_array = []
+  array = []
 end
 
 def create_an_array
-my_array = ["Veniel", "Boum", "nou", "veo"]
+array = ["Veniel", "Boum", "nou", "veo"]
 end
 
 def add_element_to_end_of_array(array, element)
-my_array = ["Veniel", "Boum", "nou", "veo"]
-my_array.push("element")
+array = ["Veniel", "Boum", "nou", "veo"]
+array.push("element")
 end
 
 def add_element_to_start_of_array(array, element)
-my_array.unshift("element")
+array.unshift("element")
 end
 
 def remove_element_from_end_of_array(array)
-my_array.pop("")
+array.pop("")
 end
 
 def remove_element_from_start_of_array(array)
-my_array.shift("")
+array.shift("")
 end
 
 def retrieve_element_from_index(array, index_number)
-my_arrayy[]
+arrayy[]
 end
 
 def retrieve_first_element_from_array(array)
-my_array[0]
+array[0]
 end
 
 def retrieve_last_element_from_array(array)
-my_array[-1]
+array[-1]
 end
